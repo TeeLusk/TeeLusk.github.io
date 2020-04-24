cc6e1435d5b2f9b3257f2186dda64833a56c7217
TeeLusk <44889927+TeeLusk@users.noreply.github.com>
2020-04-24 12:33:49 -0600

    Assets folder structure (JS, CSS, Images)

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//TODO year is pulled using javascript
+let year = new Date();
+let currentYear = year.getFullYear();
+console.log(currentYear);
+
+
+
+//TODO last updated to pull from github last commmit
