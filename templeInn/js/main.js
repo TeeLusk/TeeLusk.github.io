3656195a1b702fc2d377924842e511af4b66675e
TeeLusk <44889927+TeeLusk@users.noreply.github.com>
2020-07-17 07:45:02 -0600

    Reservation/Temples page done-ish

--- templeInn/js/main.js
+++ templeInn/js/main.js
@@ -8,90 +8,118 @@ hambutton.addEventListener('click', () => {
 }, false);
 
 
-// ----------------------------------------
-// CURRENT WEATHER FETCH
-
-
-
-
-
 
 // ----------------------------------------
 // TEMPLES
 if (document.getElementById('templesPage')) {
-const templeRequest = './js/templeData.json';
+    const templeRequest = './js/templeData.json';
 
-fetch(templeRequest)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error('Error with inital response')
-    })
-    .then(function (jsonObject) {
-        const templeList = jsonObject['temples'];
-        // console.log(templeList);
+    fetch(templeRequest)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Error with inital response')
+        })
+        .then(function (jsonObject) {
+            const templeList = jsonObject['temples'];
+            // console.log(templeList);
 
-        templeList.forEach(element => {
-            let div = document.createElement('div');
-            div.className = "row";
-            let flexSection = document.createElement('div');
-            let textSection = document.createElement('div');
-            let img = document.createElement('img');
-            let h2 = document.createElement('h2');
-            let address = document.createElement('ul');
-            let milestones = document.createElement('ul');
-            let services = document.createElement('ul');
-            let email = document.createElement('p');
-            let phone = document.createElement('p');
-            let notice = document.createElement('p');
-            let link = document.createElement('p');
+            templeList.forEach(element => {
+                let div = document.createElement('div');
+                div.className = "row";
+                let flexSection = document.createElement('div');
+                flexSection.className = 'flexSection';
+                let textSection = document.createElement('div');
+                textSection.className = 'textSection';
+                let img = document.createElement('img');
+                let h2 = document.createElement('h2');
+                let address = document.createElement('ul');
+                let milestones = document.createElement('ul');
+                let services = document.createElement('ul');
+                let email = document.createElement('p');
+                let phone = document.createElement('p');
+                let notice = document.createElement('p');
+                notice.className = "noticeText";
+                let link = document.createElement('p');
+                let weather = document.createElement('p');
 
-            address.innerHTML = '<h3>Contact Information</h3>';
-            milestones.innerHTML = '<h3>Milestones</h3>';
-            services.innerHTML = '<h3>Services</h3>';
+                address.innerHTML = '<h3>Contact Information</h3>';
+                milestones.innerHTML = '<h3>Milestones</h3>';
+                services.innerHTML = '<h3>Services</h3>';
 
-            img.src = element.photo;
-            h2.textContent = element.name;
-            email.textContent = element.email;
-            phone.textContent = element.telephone;
-            notice.textContent = element.notice;
-            link.innerHTML = '<p>For additional information please visit the Church\'s Wesbite page <a href="' + element.link +'">here</a></p>'
+                img.src = element.photo;
+                img.setAttribute('alt', `${element.name} Temple`);
+                h2.textContent = element.name;
+                email.textContent = element.email;
+                phone.textContent = element.telephone;
+                notice.textContent = element.notice;
+                link.innerHTML = '<p>For additional information please visit the Church\'s Wesbite page <a href="' + element.link + '">here</a></p>'
 
-            //Address
-            element.address.forEach(element => {
-                address.innerHTML += '<li>' + element + '</li>';
+                //Address
+                element.address.forEach(element => {
+                    address.innerHTML += '<li>' + element + '</li>';
+                });
+
+                //Milestones
+                element.milestones.forEach(element => {
+                    milestones.innerHTML += '<li>' + element + '</li>';
+                });
+
+                //Services
+                element.services.forEach(element => {
+                    services.innerHTML += '<li>' + element + '</li>';
+                });
+
+                // ----------------------------------------
+                // CURRENT WEATHER FETCH
+                const currentURL = `https://api.openweathermap.org/data/2.5/weather?id=${element.cityID}&units=imperial&appid=4447a91324b56ff7dc35262e118be15a`;
+                fetch(currentURL)
+                    .then(function (response) {
+                        if (response.ok) {
+                            return response.json();
+                        }
+                        throw new ERROR('Network response was not ok');
+                    })
+                    .then(function (jsonObject) {
+                        const weatherCurrent = jsonObject;
+
+                        let currentCondition = weatherCurrent.weather[0].main;
+                        let currentTemp = weatherCurrent.main.temp;
+                        let currentHumidity = weatherCurrent.main.humidity;
+                        let currentWindSpeed = weatherCurrent.wind.speed;
+                        let windChill = Math.floor(35.74 + 0.6215 * currentTemp - 35.75 * Math.pow(currentWindSpeed, 0.16) + 0.4275 *
+                            currentTemp * Math.pow(currentWindSpeed, 0.16));
+                        windChill = (windChill > currentTemp) ? currentTemp : windChill;
+
+                        weather.innerHTML = '<h3>Current Weather Conditions</h3><p>' + currentCondition + ', ' + currentTemp + '&deg; F</p>';
+
+                    })
+                    .catch(function (error) {
+                        console.log('Fetch error: ', error.message);
+                    });
+
+                //--------------------------------------------
+                // APPEND CHILDREN
+                div.appendChild(h2);
+                flexSection.appendChild(img);
+
+                textSection.appendChild(address);
+                textSection.appendChild(phone);
+                textSection.appendChild(email);
+
+                flexSection.appendChild(textSection);
+                div.appendChild(flexSection);
+
+
+                div.appendChild(weather);
+                div.appendChild(services);
+                div.appendChild(milestones);
+                div.appendChild(notice);
+                div.appendChild(link);
+                
+                document.getElementById('templesPage').appendChild(div);
             });
 
-            //Milestones
-            element.milestones.forEach(element => {
-                milestones.innerHTML += '<li>' + element + '</li>';
-            });
-
-            //Services
-            element.services.forEach(element => {
-                services.innerHTML += '<li>' + element + '</li>';
-            });
-
-
-
-            textSection.appendChild(address);
-            textSection.appendChild(phone);            
-            textSection.appendChild(email);
-            textSection.appendChild(services);            
-            textSection.appendChild(milestones);
-            textSection.appendChild(notice);
-            textSection.appendChild(link);
-
-            flexSection.appendChild(img);
-            flexSection.appendChild(textSection);
-
-            div.appendChild(h2);
-            div.appendChild(flexSection);
-
-            document.getElementById('templesPage').appendChild(div);
-
-        });
-
-    })
+        })
 }
