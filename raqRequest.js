a8c4d766978793bd7ccac797f2d3b99a400e2e5c
TeeLusk <TeeLusk@users.noreply.github.com>
2020-08-04 16:44:15 -0600

    Current problem: Too many processes. Crashes

--- raqRequest.js
+++ raqRequest.js
@@ -2,21 +2,28 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 let data = require('./allProductPages.json');
 
-async function checkPage(link) {
-    const browser = await puppeteer.launch();
-    let result;
-    try {
-        let page = await browser.newPage();
-        page.goto(link);
-        let result
-    } catch (err) {
-        console.log(err);
-    }
-    return ;
-}
+async function checkPageRAQ(link) {
+    const broswer = await puppeteer.launch();
+    let page = await broswer.newPage();
+    await page.goto(link);
 
+    raqSelector = '#raq_form';
+
+    if (await page.$(raqSelector) !== null) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+let resultSet = [];
 data.forEach(element => {
     element.forEach(e => {
-        let 
+        resultSet += element.filter(checkPageRAQ);
     })
-})
+    // .then((resultSet) => {
+    //     let data = JSON.stringify(resultSet);
+    //     fs.writeFileSync('allProductPages.json', data);
+    // })
+});
