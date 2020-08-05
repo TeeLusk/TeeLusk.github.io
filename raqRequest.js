09b4874ddeea13c466a7232fb4c0307df3252346
TeeLusk <TeeLusk@users.noreply.github.com>
2020-08-05 09:03:00 -0600

    Had to use reduced arrays (maxes out at 15-20ish) but it worked

--- raqRequest.js
+++ raqRequest.js
@@ -1,29 +1,46 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+// let data = require('./allProductPages.json');
 let data = require('./allProductPages.json');
+let reduced = require('./reducedPages.json');
 
 async function checkPageRAQ(link) {
-    const broswer = await puppeteer.launch();
-    let page = await broswer.newPage();
-    await page.goto(link);
+    const browser = await puppeteer.launch();
 
-    raqSelector = '#raq_form';
+    try {
+        let page = await browser.newPage();
+        await page.goto(link);
+        raqSelector = '#raq_form';
+        page.waitForSelector(raqSelector);
+        if (await page.$(raqSelector) !== null) {
+            return link;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.log(`Error processing ${link}: ${err}`);
 
-    if (await page.$(raqSelector) !== null) {
-        return true;
-    } else {
-        return false;
     }
+    await browser.close();
+
+
 };
 
-
 let resultSet = [];
-data.forEach(element => {
+reduced.forEach(element => {
     element.forEach(e => {
-        resultSet += element.filter(checkPageRAQ);
+        let singlePage = checkPageRAQ(e);
+        singlePage.then(function (result) {
+            resultSet.push(result);
+        }).then(() => {
+            let file = JSON.stringify(resultSet);
+            fs.writeFileSync('reducedFinal.json', file);
+        })
     })
-    // .then((resultSet) => {
-    //     let data = JSON.stringify(resultSet);
-    //     fs.writeFileSync('allProductPages.json', data);
-    // })
+
 });
+
+// let result = checkPageRAQ('https://www.alphagraphics.com/centers/idaho-falls-idaho-us679/products/marketing-design/graphic-design.html');
+// result.then(() => {
+//     console.log(result);
+// });
