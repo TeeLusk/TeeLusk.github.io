361809eb65bf7f29753c3a0a530a02e15d6afe5b
TeeLusk <44889927+TeeLusk@users.noreply.github.com>
2021-07-21 23:55:10 -0600

    WDD 330 Fin

--- wdd330/AgroAPI/js/main.js
+++ wdd330/AgroAPI/js/main.js
@@ -1,111 +1,18 @@
 import {
-    buildMap, changePolygon
+    buildMap,
+    changeMap
 } from "./map.js";
 
-// Import API key, other variables
-const AgroAPI = '76a3900f4cbeb05dbb772bb061d4046f';
 
-// TODO Replace with Get Polygon Call
-const polygon01 = '609b4bd4b5d88922bc02f1ab';
-const polgygon02 = '60f78e5d380af32a5a6b3cb6';
-let startTime = '1500336000';
-let endTime = '1508976000';
+    // MAIN FUNCTION CALLER
+    function main() {
+        // Initial Builds w/Default Values
+        window.addEventListener('load', buildMap);
 
 
-const basePolygonReq = 'http://api.agromonitoring.com/agro/1.0/polygons?appid=76a3900f4cbeb05dbb772bb061d4046f'
+        // Event Listeners
+        document.querySelector('#polygonSelector').addEventListener('change', changeMap);
+        document.querySelector('#imageTypeSelector').addEventListener('change', changeMap);
+    }
 
-// Build URL for Fetch
-// http://api.agromonitoring.com/agro/1.0/image/search?start={start date}&end={end date}&polyid={ID of polygon}&appid={API key}
-function buildAgroURL(api, polygonId, start, end) {
-    let url = `https://api.agromonitoring.com/agro/1.0/image/search?start=${start}&end=${end}&polyid=${polygonId}&appid=${api}`;
-    return url;
-};
-
-// Basic Fetch
-let url1 = buildAgroURL(AgroAPI, polygon01, startTime, endTime);
-console.log("url1", url1);
-
-// fetch(url1)
-//     .then(function (res) {
-//         if (res.ok) {
-//            return res.json(); 
-//         }
-//         throw new Error(`Error when trying to reach AGRO API`);
-//     })
-//     .then(function (jsonObject) {
-//         // Parse through JSON
-//         let imagesArr = [];
-//         const agroRes = jsonObject;
-//         agroRes.forEach(e => {
-//             imagesArr.push({"Img": e.image.evi2, "Stat": e.stats.ndvi});
-//         });
-//         // console.log(agroRes);
-//         // console.log(imagesArr);
-
-//         imagesDiv = document.querySelector('#images');
-//         imagesArr.forEach(e => {
-//             imagesDiv.innerHTML += `<img src=${e.Img}>
-//                                     <p>${e.Stat}</p>`
-//         // TODO Needs a second Fetch to return the stats from the specific URL
-//         // Possibly based on event?
-//         });
-
-//     })
-//     .catch(function (err) {
-//         console.log(`Fetch error: ${err.message}`);
-//     })
-
-
-function buildPolygonList() {
-    fetch(basePolygonReq)
-        .then(function (res) {
-            if (res.ok) {
-                return res.json();
-            }
-            throw new Error(`Error when trying to reach AGRO API`);
-        })
-        .then(function (jsonObject) {
-            let polygonSelect = document.querySelector('#polygonSelector');
-
-            jsonObject.forEach(e => {
-                polygonSelect.innerHTML += `<option value ='${e.id}'>${e.name}</option>`;
-            })
-
-        }).catch(function (err) {
-            console.log(`Fetch error: ${err.message}`);
-        })
-};
-
-
-function updateMap() {
-    let updatedPoly = document.querySelector('#polygonSelector').value;
-    
-    changePolygon(updatedPoly);
-}
-
-
-buildPolygonList();
-buildMap();
-
-let polygonSelect = document.querySelector('#polygon-select');
-
-document.querySelector('#polygonSelector').addEventListener('change', updateMap);
-
-// Default Value for Date Picker
-document.getElementById('datePicker').valueAsDate = new Date();
-
-
-// TODO Event listeners for Select Lists
-// Ex, on Polygon List Change, get currently selected polygonid, build URL and fetch
-// Same for image type, but with a different URL, getting currently selected polygonid & image type, and default time
-
-// Get Available Polygons for Select List
-// Get Polygon Coordinates from AGRO API for Selected Polygon
-// Plug Coordinates into Map for Marker
-// Overlay Sattelite Immagery?
-// 1. Get Image from AGRO API
-// 2. Overlay inside marker
-// Select List for Different Types of Immagery
-
-
-// TODO Time selector
+main()
--- wdd330/AgroAPI/js/map.js
+++ wdd330/AgroAPI/js/map.js
@@ -1,57 +1,122 @@
+const AgroAPI = '76a3900f4cbeb05dbb772bb061d4046f';
+let startTime = '1500336000';
+let endTime = '1508976000';
+
+
 // Build Map Function. Expects a polygon object
-var mymap = L.map('mapid').setView([42.4557, -112.5683], 13);
+export const mymap = L.map('mapid');
+
+let mapboxAPI = 'pk.eyJ1IjoidGVlbHVzayIsImEiOiJja3JjdXRjaGQwNTJ6MnZsajZrNjQ5dGppIn0.riOfHPNqBq9PEas7Mmca5Q';
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    // id: 'mapbox/satellite-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: mapboxAPI
+}).addTo(mymap);
 export function buildMap() {
+    fetch('http://api.agromonitoring.com/agro/1.0/polygons?appid=76a3900f4cbeb05dbb772bb061d4046f')
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+            throw new Error(`Error when trying to reach AGRO API`);
+        })
+        .then(function (jsonObject) {
+            let polygonSelect = document.querySelector('#polygonSelector');
+
+            jsonObject.forEach(e => {
+                polygonSelect.innerHTML += `<option value ='${e.id}'>${e.name}</option>`;
+            })
+
+        }).catch(function (err) {
+            console.log(`Fetch error: ${err.message}`);
+        })
 
 
-    // Lat/Long Switched from AgroAPI
-    // TODO Func to switch lat/long from AgroAPI
-    var arbonPolygon = L.polygon([
-        [42.423558, -112.575359],
-        [42.423558, -112.556305],
-        [42.444899, -112.556305],
-        [42.444899, -112.575359],
-        [42.423558, -112.575359]
-    ]).addTo(mymap);
-
-    let mapboxAPI = 'pk.eyJ1IjoidGVlbHVzayIsImEiOiJja3JjdXRjaGQwNTJ6MnZsajZrNjQ5dGppIn0.riOfHPNqBq9PEas7Mmca5Q';
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        // id: 'mapbox/satellite-v9',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: mapboxAPI
-    }).addTo(mymap);
+    // Fetch polygon coordinates
+    fetch(`http://api.agromonitoring.com/agro/1.0/polygons?appid=${AgroAPI}`)
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+            throw new Error(`Error when trying to reach AGRO API`);
+        })
+        .then(function (jsonObject) {
+            // Get polygon coordinates
+            let polyCoords = switchLatitiudeLongitude(jsonObject[0].geo_json.geometry.coordinates[0]);
+            // Create polygon
+            let polygon = L.polygon(polyCoords).addTo(mymap);
 
 
-    var imageUrl = "http://api.agromonitoring.com/image/1.0/000597bd000/609b4bd4b5d88922bc02f1ab?appid=76a3900f4cbeb05dbb772bb061d4046f",
-        imageBounds = [
-            [42.423558, -112.575359],
-            [42.423558, -112.556305],
-            [42.444899, -112.556305],
-            [42.444899, -112.575359],
-            [42.423558, -112.575359]
-        ];
-    L.imageOverlay(imageUrl, imageBounds).addTo(mymap);
+            fetch(`http://api.agromonitoring.com/agro/1.0/image/search?start=${startTime}&end=${endTime}&polyid=${jsonObject[0].id}&appid=${AgroAPI}`)
+                .then(function (res) {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                    throw new Error(`Error when trying to reach AGRO API`);
+                })
+                .then(function (jsonObject) {
+                    // Image Overlay
+                    let imageURL = jsonObject[0].image.truecolor;
+                    L.imageOverlay(imageURL, polyCoords).addTo(mymap);
+
+                }).catch(function (err) {
+                    console.log(`Fetch error: ${err.message}`);
+                })
+            // // Add polygon to map
+            mymap.fitBounds(polygon.getBounds());
 
 
-    // TODO Image Overlay
+        })
+        .catch(function (err) {
+            console.log(`Fetch error: ${err.message}`);
+        })
 
-    // Use to focus on active Polygon
-    mymap.fitBounds(arbonPolygon.getBounds());
+
+
+    // var imageUrl = "http://api.agromonitoring.com/image/1.0/000597bd000/609b4bd4b5d88922bc02f1ab?appid=76a3900f4cbeb05dbb772bb061d4046f",
+    //     imageBounds = [
+    //         [42.423558, -112.575359],
+    //         [42.423558, -112.556305],
+    //         [42.444899, -112.556305],
+    //         [42.444899, -112.575359],
+    //         [42.423558, -112.575359]
+    //     ];
+    // L.imageOverlay(imageUrl, imageBounds).addTo(mymap);
 
 }
 
-
-export function changePolygon(polygon) {
-    // Remove old polygon
-    if (mymap.hasLayer(polygon)) {
-        mymap.removeLayer(polygon);
-    }
-
-    // Add new polygon
-    polygon.addTo(mymap);
-    mymap.fitBounds(polygon.getBounds());
+export function switchLatitiudeLongitude(array) {
+    array.forEach(e => {
+        [e[0], e[1]] = [e[1], e[0]];
+    })
+    return array;
 }
+
+
+export function changeMap() {
+    // Get Current Values of Fields
+    let poly = document.querySelector('#polygonSelector').value;
+    let imgType = document.querySelector('#imageTypeSelector').value;
+
+    // REQ #1 - Get PolygonById for Coordinates
+
+
+    // REQ #2 - Get Image by Date and Image Type
+
+}
+
+// export function changePolygon(polygon) {
+//     // Remove old polygon
+//     if (mymap.hasLayer(polygon)) {
+//         mymap.removeLayer(polygon);
+//     }
+
+//     // Add new polygon
+//     polygon.addTo(mymap);
+//     mymap.fitBounds(polygon.getBounds());
+// }
